d54be2e70dd70763a8a919635b099ac7b2abb37e
Kyran-Calderon <kncalderon2030@clc.pshs.edu.ph>
2026-08-16 22:26:46 +0800

    The main thing

--- q1_sg2_a3.py
+++ q1_sg2_a3.py
@@ -1,1 +1,28 @@
-print("hello world!")
+Michl = int(input("Enter a year of birth not going before the year 1900.")
+if Michl < 1900:
+            print("That is an invalid year. Please input a year that is not before the year 1900.")
+            exit()
+if Mich % 12 is == 4:
+  print("Your Chinese Zodiac sign is: Rat (鼠 / Shǔ)"
+if Mich % 12 is == 5:
+  print("Your Chinese Zodiac sign is: Ox (牛 / Niú)"
+if Mich % 12 is == 6:
+  print("Your Chinese Zodiac sign is: Tiger (虎 / Hǔ)"
+if Mich % 12 is == 7:
+  print("Your Chinese Zodiac sign is: Rabbit (兔 / Tù)"
+if Mich % 12 is == 8:
+  print("Your Chinese Zodiac sign is: Dragon (龙 / Lóng)"
+if Mich % 12 is == 9:
+  print("Your Chinese Zodiac sign is: Snake (蛇 / Shé)"
+if Mich % 12 is == 10:
+  print("Your Chinese Zodiac sign is: Horse (马 / Mǎ)"
+if Mich % 12 is == 11:
+  print("Your Chinese Zodiac sign is: Goat (羊 / Yáng)"
+if Mich % 12 is == 0:
+  print("Your Chinese Zodiac sign is: Monkey (猴 / Hóu)"
+if Mich % 12 is == 1:
+  print("Your Chinese Zodiac sign is: Rooster (鸡 / Jī)"
+if Mich % 12 is == 2:
+  print("Your Chinese Zodiac sign is: Dog (狗 / Gǒu)"
+if Mich % 12 is == 3:
+  print("Your Chinese Zodiac sign is: Pig (猪 / Zhū)"
