4eb473d0bc7818a9c4cfc5664686cd26461146ef
Kyran-Calderon <kncalderon2030@clc.pshs.edu.ph>
2026-09-04 22:39:34 +0800

    FA6 Fix

--- FA6.py
+++ FA6.py
@@ -1,18 +1,18 @@
 class Lab:
-  def __init__ (self, room_number)
-  self.room_number = room_number
+  def __init__ (self, room_number):
+   self.room_number = room_number
 
 class Technician:
-  def __init__ (self, name)
-  self.name = name
-  self.assigned_lab = none
+  def __init__ (self, name):
+   self.name = name
+   self.assigned_lab = None
 
-def assign_lab(self, lab_obj):
-  self.assigned_lab = lab_obj
+  def assign_lab(self, lab_obj):
+   self.assigned_lab = lab_obj
 
 chem_lab = Lab("302")
-mr_cruz = technician("mr. Cruz")
+mr_cruz = Technician("mr. Cruz")
 
 mr_cruz.assign_lab(chem_lab)
 
-print(mr_cruz.assigned.lab.room_number)
+print(mr_cruz.assigned_lab.room_number)
